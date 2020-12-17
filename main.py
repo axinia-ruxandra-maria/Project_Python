62e925b43d45ff4176b19d7a413a93c7d5ca0d6d
axinia-ruxandra-maria <61487931+axinia-ruxandra-maria@users.noreply.github.com>
2020-12-17 15:15:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-import sys
-#citeste si salveaza intr-o lista liniile fisierului
-def lista_fisier(fisier):
-    file = open(fisier,'r')
-    lista = file.readlines()
-    file.close()
-    return lista
-
-#trece prin lista cu toate liniile unui fisier
-def extrage_linie(lista_linii,fisier):
-    for linie in lista_linii:
-        extrage_propozitie(linie.strip(),fisier)
-
-#salveaza intr-o lista fiecare propozitie dintr-o linie
 def extrage_propozitie(linie,fisier):
     global propozitii1
     global propozitii2
@@ -31,36 +17,44 @@ def extrage_propozitie(linie,fisier):
                 extrage_propozitie(new_sentence,"file2")
 
 def compara_propozitii(propozitie1, propozitie2):
-    if len(propozitie1) != len(propozitie2):
+    prop1 = propozitie1.replace(" ", "")
+    prop2 = propozitie2.replace(" ", "")
+    if len(prop1) != len(prop2):
         return False
     else:
          i = 0
-         while i < len(propozitie1):
-             if propozitie1[i] != propozitie2[i]:
+         while i < len(prop1):
+             if prop1[i] != prop2[i]:
                  return False
              i = i + 1
     return True
 
-def creeaza_lista_comuna(propozitii1, propozitii2, final_list):
+def creeaza_lista_comuna(propozitii1,propozitii2,final_list):
     for prop1 in propozitii1:
         for prop2 in propozitii2:
-            if compara_propozitii(prop1, prop2):
-                final_list.append(prop1)
+            if compara_propozitii(prop1,prop2):
+                if prop2 not in final_list:
+                    final_list.append(prop2)
+
+def lista_fisier(fisier):
+    file = open(fisier,'r')
+    lista = file.readlines()
+    file.close()
+    return lista
+
+def extrage_linie(lista_linii,fisier):
+    for linie in lista_linii:
+        extrage_propozitie(linie.strip(),fisier)
+
+lista_linii_f1 = lista_fisier("file1.txt")
+lista_linii_f2 = lista_fisier("file2.txt")
 
 propozitii1 =[]
 propozitii2=[]
 final_list = []
 
-if len(sys.argv) != 3:
-    print("Wrong number of arguments !")
-else:
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    lista_linii_f1 = lista_fisier(file1)
-    lista_linii_f2 = lista_fisier(file2)
+extrage_linie(lista_linii_f1,"file1")
+extrage_linie(lista_linii_f2,"file2")
 
-    extrage_linie(lista_linii_f1,"file1")
-    extrage_linie(lista_linii_f2,"file2")
-
-    creeaza_lista_comuna(propozitii1,propozitii2,final_list)
-    print(final_list)
+creeaza_lista_comuna(propozitii1,propozitii2,final_list)
+print(final_list)
